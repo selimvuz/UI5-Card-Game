942da1edcd4652907aeb61c7e5a1223624dfc157
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-10-31 21:11:56 +0300

    Modern anlamda clean code denemesi

--- card-game/webapp/controller/MainView.controller.js
+++ card-game/webapp/controller/MainView.controller.js
@@ -6,7 +6,6 @@ function (Controller) {
 
     return Controller.extend("cardgame.cardgame.controller.MainView", {
         onInit: function() {
-            // LocalStorage'da kullanıcı adı kontrolü
             var username = localStorage.getItem("username");
             if (username) {
                 this.byId("usernameInput").setValue(username);
@@ -14,16 +13,12 @@ function (Controller) {
         },
 
         onStartGame: function() {
-            // Kullanıcı adını ve zorluk seviyesini al
             var username = this.byId("usernameInput").getValue();
             var difficulty = this.byId("difficultySelect").getSelectedKey();
 
             if (username) {
-                // Kullanıcı adını localStorage'a kaydet
                 localStorage.setItem("username", username);
 
-                // Zorluk seviyesine göre oyuna başla
-                // Oyun ayarlarını başlatan bir fonksiyonu çağırabilirsin
                 this.startGame(difficulty);
             } else {
                 sap.m.MessageToast.show("Lütfen kullanıcı adınızı girin.");
@@ -31,8 +26,7 @@ function (Controller) {
         },
 
         startGame: function(difficulty) {
-            // Zorluk seviyesine göre gerekli ayarlamaları yap ve oyunu başlat
-            // Örneğin kartları yerleştirme, timer başlatma vb. işlemler burada olabilir
+
         }
     });
 });
