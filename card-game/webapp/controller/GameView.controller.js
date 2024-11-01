4496c12a6b91411948f4013211e757be84916a7a
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-11-02 02:28:22 +0300

    Kartların çift gelmesi, temel flip mantığı, oyun sesleri

--- card-game/webapp/controller/GameView.controller.js
+++ card-game/webapp/controller/GameView.controller.js
@@ -6,6 +6,7 @@ sap.ui.define([
 
     return Controller.extend("cardgame.cardgame.controller.GameView", {
         onInit: function () {
+            this.selectedCards = [];
             this.startGame();
         },
 
@@ -21,6 +22,7 @@ sap.ui.define([
                 case "easy":
                     pokemonCount = 4;
                     gridSettings = {defaultSpan: "L3 M6 S12"};
+                    break;
                 case "medium":
                     pokemonCount = 6;
                     gridSettings = {defaultSpan: "L4 M6 S12"};
@@ -31,31 +33,48 @@ sap.ui.define([
                     break;
             }
 
+            var pokemonList = [];
+            for (var i = 0; i < pokemonCount; i++) {
+                pokemonList.push(i + 1);
+                pokemonList.push(i + 1);
+            }
+
+            pokemonList = this.shuffleArray(pokemonList);
+
             var grid = new Grid({
                 defaultSpan: gridSettings.defaultSpan
             });
 
-            for (var i = 0; i < pokemonCount; i++) {
-                var card = new sap.m.Panel({
-                    content: [
-                        new sap.m.Image({
-                            src: "../assets/pokemon" + (i + 1) + ".jpg",
-                            width: "100px",
-                            height: "140px"
-                        })
-                    ],
-                    headerText: "Pokemon " + (i + 1),
-                    width: "100%",
-                    height: "auto",
-                    addStyleClass: "pokemon-card"
+            for (var j = 0; j < pokemonList.length; j++) {
+                var cardHTML = new sap.ui.core.HTML({
+                    content: `
+                        <div class="pokemon-card" id="card-${j}" data-card-id="${pokemonList[j]}" onclick="sap.ui.getCore().byId('${this.getView().getId()}').getController().onCardClick(this)">
+                            <div class="pokemon-card-inner">
+                                <div class="pokemon-card-front">
+                                    <img src="../assets/pokemon-back.png" width="100px" height="150px" />
+                                </div>
+                                <div class="pokemon-card-back">
+                                    <img src="../assets/pokemon${pokemonList[j]}.jpg" width="100px" height="150px" />
+                                </div>
+                            </div>
+                        </div>
+                    `
                 });
 
-                grid.addContent(card);
+                grid.addContent(cardHTML);
             }
 
             gameArea.addItem(grid);
 
             this.startTimer();
+        },
+
+        shuffleArray: function (array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
         },
 
         startTimer: function () {
@@ -66,6 +85,32 @@ sap.ui.define([
                 time++;
                 timerDisplay.setText("Zaman: " + time + " saniye");
             }, 1000);
-        }
+        },
+
+        checkForMatch: function () {
+            var card1 = this.selectedCards[0];
+            var card2 = this.selectedCards[1];
+        
+            if (card1.dataset.cardId === card2.dataset.cardId) {
+                this.selectedCards = [];
+            } else {
+                setTimeout(function () {
+                    card1.classList.remove("flipped");
+                    card2.classList.remove("flipped");
+                    this.selectedCards = [];
+                }.bind(this), 1000);
+            }
+        },
+
+        onCardClick: function (cardElement) {
+            if (!cardElement.classList.contains("flipped")) {
+                cardElement.classList.add("flipped");
+                this.selectedCards.push(cardElement);
+
+                if (this.selectedCards.length === 2) {
+                    this.checkForMatch();
+                }
+            }
+        }        
     });
 });
--- card-game/webapp/controller/MainView.controller.js
+++ card-game/webapp/controller/MainView.controller.js
@@ -6,6 +6,16 @@ function (Controller) {
 
     return Controller.extend("cardgame.cardgame.controller.MainView", {
         onInit: function() {
+            this.menuMusic = new Audio(sap.ui.require.toUrl("cardgame/cardgame/assets/sounds/sound1.mp3"));
+            this.gameMusic = new Audio(sap.ui.require.toUrl("cardgame/cardgame/assets/sounds/sound2.mp3"));
+        
+            this.menuMusic.loop = true;
+            this.menuMusic.volume = 0.4;
+            this.menuMusic.play(); 
+
+            this.gameMusic.loop = true;
+            this.gameMusic.volume = 0.5;
+            
             var username = localStorage.getItem("username");
             if (username) {
                 this.byId("usernameInput").setValue(username);
@@ -15,13 +25,16 @@ function (Controller) {
         onStartGame: function() {
             var username = this.byId("usernameInput").getValue();
             var difficulty = this.byId("difficultySelect").getSelectedKey();
+
+            this.menuMusic.pause();
+            this.gameMusic.play();
             
             if (username) {
                 localStorage.setItem("username", username);
                 localStorage.setItem("difficulty", difficulty);
                 
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                oRouter.navTo("game");
+                oRouter.navTo("game", {}, true);
             } else {
                 sap.m.MessageToast.show("Lütfen kullanıcı adınızı girin.");
             }
